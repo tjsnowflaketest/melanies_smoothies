b07fbcff5709392cfabbd742555723424ff5334a
tjsnowflaketest <travisjjoel+git@gmail.com>
2025-11-22 03:23:08 +0000

    Fixing connection info

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
 st.write(
@@ -12,11 +13,11 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cns = st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 
-session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe,use_container_width=True)
 
