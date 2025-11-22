109701daff0e352a1a9599f344394f6879e6998e
tjsnowflaketest <travisjjoel+git@gmail.com>
2025-11-22 03:31:16 +0000

    Adding API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success(name_on_order +", " +'Your Smoothie is ordered!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
